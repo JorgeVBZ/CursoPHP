acdc7a34fe023228adc77d2febd3ce67cabca020
JorgeVBZ <jvillacastin@bitherm.com>
2020-08-28 12:15:44 +0200

    Practica 1 Cookies

---
+++ Sesiones & Cookies/Practica_1/english.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>WELCOME TO MY WEBSITE :D</h1>
+<table width="800" border="0">
+<tr>
+    <td><img src="images/smacks.jpg" width="300" height="400"></td>
+    <td><img src="images/mickey.jpg" width="300" height="400"></td>
+</tr>
+<tr>
+    <td><img src="images/minion.jpg" width="300" height="166"></td>
+    <td><img src="images/linux.jpg" width="300" height="171"></td>
+</tr>
+</body>
+</html>
