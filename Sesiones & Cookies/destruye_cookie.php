a0e0b0bce6f3d397874cbf6c2c0c12fdf1da397b
JorgeVBZ <jvillacastin@bitherm.com>
2020-08-28 11:23:31 +0200

    Sesiones y Cookies

---
+++ Sesiones & Cookies/destruye_cookie.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+
+setcookie("prueba","Esta es la información de nuestra primera cookie",time()-1,"/CursoPHP/Sesiones%20&%20Cookies/zona_contenidos/");
+
+
+
+?>
+
+</body>
+</html>
