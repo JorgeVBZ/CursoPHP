e06340c2dcba778c265012d363c1575950f6123a
JorgeVBZ <jvillacastin@bitherm.com>
2020-09-03 12:23:42 +0200

    Correos electronicos

---
+++ Correos_electronicos/enviar_mail.php
@@ -0,0 +1,30 @@
+<?php
+
+    if($_POST["nombre"] == "" || $_POST["apellido"] == "" || $_POST["email"] == "" || $_POST["comentarios"] == ""){
+
+        echo "Completa los campos requeridos.";
+
+        die();
+    }
+
+    $texto_mail=$_POST["comentarios"];
+
+    $destinatario=$_POST["email"];
+
+    $asunto=$_POST["asunto"];
+
+    $headers="MIME-Version: 1.0\r\n";
+
+    $headers.= "Content-type: text/html; charset=utf8\r\n";
+
+    $headers.= "From: Prueba Jorge <jorge.espia@hotmail.es>\r\n";
+
+    $exito = mail($destinatario,$asunto,$texto_mail,$headers);
+
+    if($exito){
+        echo "Mensaje enviado con éxito";
+    }else{
+        echo "No se ha podido enviar el mensaje.";
+    }
+
+?>
